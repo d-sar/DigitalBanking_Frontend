33c2f42ba6b8376f3324971dd366dd4d81cced3f
d-sar <damrisara1@gmail.com>
2025-05-14 21:05:58 +0100

    ajouter new customer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {CustomersComponent} from './customers/customers.component';
 import {AccountsComponent} from './accounts/accounts.component';
 import {NgModule} from '@angular/core';
+import {NewCustomerComponent} from './new-customer/new-customer.component';
 
 export const routes: Routes = [
   {path :"customers",component :CustomersComponent},
   {path :"accounts",component :AccountsComponent},
+  {path :"new-customer",component : NewCustomerComponent},
 
 ];
 @NgModule({
